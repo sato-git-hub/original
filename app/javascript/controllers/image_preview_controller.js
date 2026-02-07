be77de15b5f2908c698aadd9d0e191e392f6d77f
sato-git-hub <mimilili.disney@gmail.com>
2026-02-07 20:26:10 +0900

    途中作業

---
+++ app/javascript/controllers/image_preview_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["preview"]
+  static values = {
+    width: { type: String, default: "150px" }, 
+    height: { type: String, default: "auto" },
+    fit: { type: String, default: "cover" },
+    position: { type: String, default: "center" }
+  }
+
+  preview(event) {
+    const files = event.target.files
+
+    if(!files) return;
+      this.previewTarget.innerHTML = ""
+    Array.from(files).forEach(file=>{
+    if (!file.type.startsWith("image/")) return;
+
+    const imageUrl = URL.createObjectURL(file);
+    const img = document.createElement("img")
+    img.src = imageUrl
+    img.style.width = this.widthValue;
+    img.style.height = this.heightValue;
+    img.style.objectFit = this.fitValue;
+    img.style.objectPosition = this.positionValue;
+    img.classList.add("rounded");
+    img.onload = () => {
+      URL.revokeObjectURL(imageUrl); 
+    };
+  this.previewTarget.appendChild(img)
+    })
+}
+}
