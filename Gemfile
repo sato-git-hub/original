8da97becf6132e980b8a59e62e4e6cd8873b55b9
sato-git-hub <mimilili.disney@gmail.com>
2026-02-17 02:10:59 +0900

    途中作業

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem "natto"
 gem "ransack"
 gem "sidekiq"
 gem "payjp"
+gem "aws-sdk-s3", require: false
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,9 @@
 redis_url = ENV.fetch("REDIS_URL")
-# Sidekiq が使う Redis の接続先を指定 実行と登録先を統一
-# ジョブを実行する側
+
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
 end
-# ジョブを登録する側
+
 Sidekiq.configure_client do |config|
   config.redis = { url: redis_url }
 end
