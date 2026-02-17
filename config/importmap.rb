a173f77732a9a0084e5257117184d863268612d0
sato-git-hub <mimilili.disney@gmail.com>
2026-02-18 00:20:38 +0900

    途中作業

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@9.4.1/swiper-bundle.esm.browser.min.js"
+# 追加
+pin "swiper_setup", preload: true
